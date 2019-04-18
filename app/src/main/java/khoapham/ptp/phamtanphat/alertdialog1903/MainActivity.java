3ceb5602ddf3b5cba41fdcf4715bbb4d14d38c5c
Phạm Tấn Phát <phatdroid94@gmail.com>
2019-04-18 21:31:59 +0700

    a

--- app/src/main/java/khoapham/ptp/phamtanphat/alertdialog1903/MainActivity.java
+++ app/src/main/java/khoapham/ptp/phamtanphat/alertdialog1903/MainActivity.java
@@ -1,13 +1,31 @@
 package khoapham.ptp.phamtanphat.alertdialog1903;
 
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button btnAlertDialog;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnAlertDialog = findViewById(R.id.buttonAlertDialog);
+
+        btnAlertDialog.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+                builder.setTitle("Hộp thoại thông báo");
+                builder.setMessage("Bạn muốn thoát hay không");
+                builder.setIcon(android.R.drawable.star_on);
+                builder.show();
+            }
+        });
     }
 }
---
+++ app/src/main/java/khoapham/ptp/phamtanphat/alertdialog1903/Person.java
@@ -0,0 +1,37 @@
+package khoapham.ptp.phamtanphat.alertdialog1903;
+
+public class Person {
+    private String ten;
+    private String tuoi;
+
+    private Person(Builder builder) {
+        this.ten = builder.ten;
+        this.tuoi = builder.tuoi;
+    }
+
+    static class Builder{
+        public String ten;
+        public String tuoi;
+
+        public String getTen() {
+            return ten;
+        }
+
+        public Builder setTen(String ten) {
+            this.ten = ten;
+            return this;
+        }
+
+        public String getTuoi() {
+            return tuoi;
+        }
+
+        public Builder setTuoi(String tuoi) {
+            this.tuoi = tuoi;
+            return this;
+        }
+        public Builder create(){
+            return new Builder();
+        }
+    }
+}
