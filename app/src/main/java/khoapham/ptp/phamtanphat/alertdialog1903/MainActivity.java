8a12a6210bea4c5597496fb9e27a3624271b637a
Phạm Tấn Phát <phatdroid94@gmail.com>
2019-04-20 20:07:55 +0700

    a

--- app/src/main/java/khoapham/ptp/phamtanphat/alertdialog1903/MainActivity.java
+++ app/src/main/java/khoapham/ptp/phamtanphat/alertdialog1903/MainActivity.java
@@ -1,5 +1,6 @@
 package khoapham.ptp.phamtanphat.alertdialog1903;
 
+import android.content.DialogInterface;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -20,10 +21,31 @@ public class MainActivity extends AppCompatActivity {
         btnAlertDialog.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+                final AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
                 builder.setTitle("Hộp thoại thông báo");
                 builder.setMessage("Bạn muốn thoát hay không");
+                builder.setCancelable(false);
+
                 builder.setIcon(android.R.drawable.star_on);
+                builder.setPositiveButton("Co", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.cancel();
+
+                    }
+                });
+                builder.setNegativeButton("Khong", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.cancel();
+                    }
+                });
+                builder.setNeutralButton("Huy", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.cancel();
+                    }
+                });
                 builder.show();
             }
         });
